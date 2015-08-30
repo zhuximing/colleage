78311caee4772a0ab2105f192923d7c2560b7ebc
zhuximing <569591357@qq.com>
2015-08-30 18:44:31 +0800

    adddddd

---
+++ colleage/colleage/Login.h
@@ -0,0 +1,13 @@
+//
+//  Login.h
+//  colleage
+//
+//  Created by Apple on 15/8/30.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Login : NSObject
+
+@end
---
+++ colleage/colleage/Login.m
@@ -0,0 +1,13 @@
+//
+//  Login.m
+//  colleage
+//
+//  Created by Apple on 15/8/30.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import "Login.h"
+
+@implementation Login
+
+@end
